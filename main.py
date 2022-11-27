f2fe432fe650e3982bc42da3daa8f6f15c16f232
fgygfdfg <115881073+fgygfdfg@users.noreply.github.com>
2022-11-27 12:50:32 +0800

    MakeCode 專案的初始檔案

---
+++ main.py
@@ -0,0 +1,5 @@
+basic.show_number(0)
+datalogger.create_cv("42", None)
+def on_button_pressed_a():
+    pass
+input.on_button_pressed(Button.A, on_button_pressed_a)
---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.showNumber(0)
+datalogger.createCV("42", null)
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+})
